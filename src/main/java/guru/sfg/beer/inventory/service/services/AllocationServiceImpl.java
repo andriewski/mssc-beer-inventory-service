22376279dafe6b04450ce3d995e7687fd30ee982
Mark <plambiroo@gmail.com>
2020-08-06 15:35:16 +0300

    fix bug with quantity on hand

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -75,7 +75,9 @@ public class AllocationServiceImpl implements AllocationService {
             } else if (inventory > 0) { //partial allocation
                 beerOrderLine.setQuantityAllocated(allocatedQuantity + inventory);
                 beerInventory.setQuantityOnHand(0);
+            }
 
+            if (beerInventory.getQuantityOnHand() == 0) {
                 beerInventoryRepository.delete(beerInventory);
             }
         });
